e3bdd77422bf77f57d16e14d82cfa36346f229f5
Drozdov Igor <igo.drozdow@gmail.com>
2016-08-27 16:15:20 +0600

    pizdos

--- pRNQlDmQ.js
+++ pRNQlDmQ.js
@@ -8,17 +8,39 @@
 
 var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game', { preload: preload, create: create, update: update, render: render });
 
+
+
 function preload() {
 
     game.stage.backgroundColor = '#85b5e1';
+    game.load.crossOrigin = "anonymous";
 
+	// game.load.json('gameData','http://phaser.io/version.json');
+	// game.cache.getJSON('gameData').
+	game.load.image('einstein', './assets/pics/test.jpg');
+	/*game.gameData = game.cache.getJSON('gameData');
+     this.assets = this.game.gameData.assets;
 
+     this.assets.forEach(function(item){
+       switch (item.type) {
+	case "image":
+	     this.load.image(item.label, item.source);
+	     break;	
+	}	
+},this);
+*/
 }
 
 
 function create() {
 
-    
+     game.stage.backgroundColor = '#0072bc';
+
+     
+
+ 	game.add.sprite(0, 0, 'einstein');
+
+
 
 }
 
