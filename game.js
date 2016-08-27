86f8106dabac6a748f5e15a64c0a102286ce5f12
Drozdov Igor <igo.drozdow@gmail.com>
2016-08-27 22:10:54 +0600

    HealthBar

--- game.js
+++ game.js
@@ -19,6 +19,8 @@ function preload() {
 
 	devices.forEach(loadDevice);
 
+	this.game.load.spritesheet('button', 'assets/buttons/plus_minus.png', 31, 31);
+
 }
 
 
@@ -28,6 +30,21 @@ function create() {
 	});
 	
     new dist(game, 100, 100);
+
+
+// ----------------------------------------------
+
+	var barre1_x = 300;
+    var barre1_y = 300;
+    this.healthValue = 100;
+ 	this.myHealthBar = new HealthBar(this.game, {x: barre1_x, y: barre1_y});
+ 	this.minusButton = this.game.add.button(barre1_x - 50, barre1_y + 30, 'button', onMinusClick, this, 1, 1, 1, 1);
+    this.plusButton = this.game.add.button(barre1_x , barre1_y + 30, 'button', onPlusClick, this, 0);
+
+
+
+// -----------------------------------------------
+
      
     var phaserJSON = game.cache.getJSON('gameData');
      console.log(phaserJSON.level[0]);
@@ -48,3 +65,16 @@ function loadDevice(name){
 	game.load.text(name, '/assets/devices/' + name + '/' + name + '.js');
     game.load.image(name, 'assets/devices/' + name + '/' + name + '.png');
 }
+
+function onPlusClick(){
+      this.healthValue = this.healthValue + 10;
+      if(this.healthValue > 100) this.healthValue = 100;
+      this.myHealthBar.setPercent(this.healthValue);
+}
+
+function onMinusClick(){
+      this.healthValue = this.healthValue - 10;
+      if(this.healthValue < 0) this.healthValue = 0;
+      this.myHealthBar.setPercent(this.healthValue);
+}
+
